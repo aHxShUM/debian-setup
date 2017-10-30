a1a9da58bbe6c28b459c619ee0dbe197b66fe22a
Ah Shum <ahshum911@gmail.com>
2017-10-30 09:56:16 +0800

    Update 200-install-qemu.sh

--- 200-install-qemu.sh
+++ 200-install-qemu.sh
@@ -11,8 +11,12 @@ adduser $USER libvirt-qemu
 
 # SPICE
 # https://www.spice-space.org/download.html
-# https://www.spice-space.org/download/windows/spice-guest-tools/spice-guest-tools-latest.exe
-apt-get install virt-viewer
+apt-get install virt-viewer libspice-protocol-dev libspice-server1
+
+# SPICE Guest
+#
+# Windows:
+#     https://www.spice-space.org/download/windows/spice-guest-tools/spice-guest-tools-latest.exe
 
 # Virtio
 # https://fedoraproject.org/wiki/Windows_Virtio_Drivers#Direct_download
