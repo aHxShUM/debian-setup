23de303bb31c69446602320ee2ad067bad9b2cee
Ah Shum <ahshum911@gmail.com>
2017-11-02 08:48:13 +0800

    Update 050-hibernate.sh

--- 050-hibernate.sh
+++ 050-hibernate.sh
@@ -2,3 +2,15 @@
 
 # Hibernate
 apt-get install uswsusp
+
+# Add swapfile
+fallocate -l 4G /swapfile
+mkswap /swapfile
+swapon /swapfile
+
+# Make swap persist
+echo '/swapfile none swap sw 0 0' | tee -a /etc/fstab
+
+# Use less swap
+echo 'vm.swappiness=10' | tee -a /etc/sysctl.d/swap.conf
+sysctl -p
