7d9d9669bccc22d1e528491ac9e89b446e231bc0
Ah Shum <ahshum911@gmail.com>
2017-11-04 07:39:57 +0800

    Update 005-im.sh

--- 005-im.sh
+++ 005-im.sh
@@ -1,13 +1,5 @@
 #!/bin/sh
 
-# Fonts
-apt-get install \
-  fonts-arphic-ukai \
-  fonts-arphic-uming \
-  fonts-ipafont-mincho \
-  fonts-ipafont-gothic \
-  fonts-unfonts-core
-
 # Input method
 # choices : ibus, scim, fcitx
 apt-get install ibus-cangjie
