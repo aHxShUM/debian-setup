34aad015bcfcff7b27464326b89f33c44b110195
Ah Shum <ahshum911@gmail.com>
2017-11-12 07:06:51 +0800

    Update 500-xinitrc.sh

--- 500-xinitrc.sh
+++ 500-xinitrc.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
 
+#
+# DPMS
+#   https://wiki.archlinux.org/index.php/Display_Power_Management_Signaling#Modifying_DPMS_and_screensaver_settings_using_xset
+#
+
 echo 'xset r rate 250 50
+xset s off
+xset dpms 0 0 0
 . /etc/X11/Xsession' | tee ~/.xinitrc
