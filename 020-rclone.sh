85b7ef9c67b9711839e5019b3f18eba48ac477ab
Ah Shum <ahshum911@gmail.com>
2017-11-11 12:01:54 +0800

    Create 020-rclone.sh

---
+++ 020-rclone.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# download
+curl -O https://downloads.rclone.org/rclone-current-linux-amd64.zip
+7z x rclone-current-linux-amd64.zip
+cd rclone-*-linux-amd64
+
+# copy binary
+sudo cp rclone /usr/bin/
+sudo chown root:root /usr/bin/rclone
+sudo chmod 755 /usr/bin/rclone
+
+# install manpage
+sudo mkdir -p /usr/local/share/man/man1
+sudo cp rclone.1 /usr/local/share/man/man1/
+sudo mandb
