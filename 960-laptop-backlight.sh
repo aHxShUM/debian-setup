f6a9b4ba345b448f39218ffee19a881e5827e5d9
Ah Shum <ahshum911@gmail.com>
2017-11-13 20:58:01 +0800

    Update 960-laptop-backlight.sh

--- 960-laptop-backlight.sh
+++ 960-laptop-backlight.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
-BRIGHTNESS=${1:-800}
+BASE=/sys/class/backlight/intel_backlight
+MAX=$(cat $BASE/max_brightness)
+PERCENT=${1:-10}
 
-echo $BRIGHTNESS | tee /sys/class/backlight/intel_backlight/brightness
+VALUE=$(($PERCENT * $MAX / 100))
+
+echo $VALUE | tee $BASE/brightness
