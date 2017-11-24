c66b032d0b14aeced8573c8da3112d4d14645d34
Ah Shum <ahshum911@gmail.com>
2017-11-24 16:58:47 +0800

    Update 500-xinitrc.sh

--- 500-xinitrc.sh
+++ 500-xinitrc.sh
@@ -8,4 +8,10 @@
 echo 'xset r rate 250 50
 xset s off
 xset dpms 0 0 0
+
+export GTK_IM_MODULE=xim
+export QT_IM_MODULE=xim
+export XMODIFIERS=@im=fcitx
+fcitx
+
 . /etc/X11/Xsession' | tee ~/.xinitrc
