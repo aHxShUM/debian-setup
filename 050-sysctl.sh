0e78f86be55ea7feb213ce11baf60d66aab9c192
Ah Shum <ahshum911@gmail.com>
2018-04-26 12:37:41 +0800

    Update 050-sysctl.sh

--- 050-sysctl.sh
+++ 050-sysctl.sh
@@ -3,5 +3,10 @@
 # fs watcher
 echo 'fs.inotify.max_user_watches = 524288' | tee /etc/sysctl.d/50-fs.inotify.conf
 
+echo 'net.bridge.bridge-nf-call-ip6tables = 0
+net.bridge.bridge-nf-call-iptables = 0
+net.bridge.bridge-nf-call-arptables = 0
+' | tee /etc/sysctl.d/10-disable-firewall-on-bridge.conf
+
 # Commit updates
-sysctl -p
+sysctl --system
