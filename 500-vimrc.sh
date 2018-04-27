0dbb6b48e129b7b00030f843e54d1d574df94462
Ah Shum <ahshum911@gmail.com>
2018-04-27 14:14:05 +0800

    Update 500-vimrc.sh

--- 500-vimrc.sh
+++ 500-vimrc.sh
@@ -5,9 +5,30 @@
 # to /etc/vim/vimrc
 # for respecting vimrc.local
 
-echo ':set mouse-=a
-:set tabstop=4 shiftwidth=4 softtabstop=4
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+echo "call plug#begin()
+
+Plug 'othree/javascript-libraries-syntax.vim'
+Plug 'cespare/vim-toml'
+Plug 'scrooloose/nerdtree'
+Plug 'tpope/vim-surround'
+Plug 'kien/ctrlp.vim'
+Plug 'airblade/vim-gitgutter'
+Plug 'altercation/vim-colors-solarized'
+Plug 'nathanaelkane/vim-indent-guides'
+Plug 'scrooloose/nerdcommenter'
+Plug 'vim-airline/vim-airline'
+Plug 'valloric/youcompleteme'
+Plug 'mattn/emmet-vim'
+Plug 'tpope/vim-repeat'
+
+call plug#end()
+
+:set mouse-=a
+:set tabstop=2 shiftwidth=2 softtabstop=2
 :set expandtab
 :set formatoptions-=cro
 :syntax on
-' | tee /etc/vim/vimrc.local
+" | tee ~/.vimrc
