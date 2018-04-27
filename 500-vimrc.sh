67bae577680a0ed4e67e3c9abe1483b4ec452d4c
Ah Shum <ahshum911@gmail.com>
2018-04-27 19:40:15 +0800

    Update 500-vimrc.sh

--- 500-vimrc.sh
+++ 500-vimrc.sh
@@ -7,7 +7,7 @@
 
 file="~/.vim/autoload/plug.vim"
 
-if [ -f "$file" ]; then
+if [ ! -f "$file" ]; then
     curl -fLo "$file" --create-dirs \
         https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 fi
