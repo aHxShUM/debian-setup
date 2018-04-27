5a055956fd042d6d52832de68926eaeabc9ef588
Ah Shum <ahshum911@gmail.com>
2018-04-27 20:45:03 +0800

    Update 200-docker.sh

--- 200-docker.sh
+++ 200-docker.sh
@@ -1,14 +1,10 @@
 #!/bin/sh
 
-USER=${1:-ahshum}
-
 # install docker
 wget -qO- get.docker.com | sh
 
-adduser $USER docker
-
 # install docker-compose
 # detail : https://github.com/docker/compose/releases
-curl -L https://github.com/docker/compose/releases/download/1.16.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+curl -L https://github.com/docker/compose/releases/download/1.21.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 
 chmod +x /usr/local/bin/docker-compose
