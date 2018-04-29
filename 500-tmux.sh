ee0723bd25ffe61a7ef3b64b9fba737029e716c8
Ah Shum <ahshum911@gmail.com>
2018-04-30 07:34:38 +0800

    Update 500-tmux.sh

--- 500-tmux.sh
+++ 500-tmux.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-echo "set -g mode-keys vi
+echo "set -g default-terminal screen-256color
+set -g mode-keys vi
 
 bind-key -t vi-copy 'y' copy-pipe 'xclip -i -sel c'
 " | tee ~/.tmux.conf
