f55aea16ed5dcb3b305496cd7fc5a6e33dc92c63
Ah Shum <ahshum911@gmail.com>
2018-04-30 23:00:52 +0800

    Update 000-essential.sh

--- 000-essential.sh
+++ 000-essential.sh
@@ -4,6 +4,8 @@ pkgs=""
 pkgs="$pkgs net-tools dnsutils wireless-tools wpasupplicant whois"
 pkgs="$pkgs xorg i3 p7zip-full firefox-esr curl sudo ranger tmux bc alsa-utils"
 pkgs="$pkgs vim-nox git jq"
-pkgs="$pkgs xclip scrot feh"
+pkgs="$pkgs xclip scrot feh python-pip"
 
 apt-get install "$pkgs"
+
+pip install mackup
