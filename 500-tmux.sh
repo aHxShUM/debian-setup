e87889c8331bc0b04b4180dc2d8b6cc9a5fb7c97
Ah Shum <ahshum911@gmail.com>
2018-04-30 17:18:30 +0800

    Update 500-tmux.sh

--- 500-tmux.sh
+++ 500-tmux.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-tee ~/.tmux.conf << EOF
+tee ~/.tmux.conf << 'EOF'
+set -g default-terminal "screen-256color"
 set -g mode-keys vi
 
 bind-key -t vi-copy 'y' copy-pipe 'xclip -i -sel c'
