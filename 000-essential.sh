c143b4335921ec40864b6ef08078786b1b0bb027
Ah Shum <ahshum911@gmail.com>
2018-05-23 16:50:49 +0800

    Update 000-essential.sh

--- 000-essential.sh
+++ 000-essential.sh
@@ -3,7 +3,7 @@
 pkgs=""
 pkgs="$pkgs net-tools dnsutils wireless-tools wpasupplicant whois"
 pkgs="$pkgs xorg i3 p7zip-full firefox-esr curl sudo ranger tmux bc alsa-utils"
-pkgs="$pkgs vim-gtk git jq"
+pkgs="$pkgs vim-gtk git jq sshuttle gimp bc"
 pkgs="$pkgs xclip scrot feh gimp python-pip"
 
 apt-get install "$pkgs"
